0fc2c82d1d84e1559e7040f8c9e2e9e5c0a6d1d9
V1k770r <testprueba066@gmail.com>
2024-05-14 12:25:39 -0500

    Practicas 1

--- src/JuegoDeAdivinanzas.java
+++ src/JuegoDeAdivinanzas.java
@@ -4,51 +4,65 @@ import java.util.Scanner;
 public class JuegoDeAdivinanzas {
     public static void main(String[] args) {
 
-
         int numeroAleatorio = new Random().nextInt(100);
         System.out.println(numeroAleatorio);
 
         Scanner teclado = new Scanner(System.in);
-        System.out.println("Escoge tu primer numero, del 1 al 100");
-        int numeroEscogido = teclado.nextInt();
 
-        if (numeroEscogido == numeroAleatorio){
-            System.out.println("Lograstes adivinar el numero aleatorio ");
-        } else {
-            System.out.println("Sigue intentando");
-        }
+        System.out.println("Bienvenido al juego adivina el numero, escoge un número del 1 al 100. Recuerda que solo tienes " +
+                "hasta 5 intentos. ");
 
-        for (int i = 1; i <= 5 ; i++) {
+        int numeroDeIntentos = 0;
 
-            switch (i) {
-                case 1:
-                    System.out.println();
+        while(numeroDeIntentos < 5){
+                System.out.println("Digita tu numero: ");
+                int numeroEscogido = teclado.nextInt();
+
+                if (numeroEscogido == numeroAleatorio){
+                    System.out.println("Lograstes adivinar el numero aleatorio, que afortunado eres!!!");
                     break;
-                case 2:
-                    System.out.println();
-                    break;
-                case 3:
-                    System.out.println();
-                    break;
-                case 4:
-                    System.out.println();
-                    break;
-                case 5:
-                    System.out.println();
-                    break;
-                default:
-                    System.out.println("No lograste adivinar el número, juego terminado");
+                } else {
+                    if (numeroEscogido > numeroAleatorio){
+                        System.out.println("El numero que digitaste es mayor al numero escondido");
+                    } else {
+                        System.out.println("El numero que digitaste es menor al numero escondido");
+                    }
+
+                }
+            numeroDeIntentos++;
+
+            if (numeroDeIntentos == 5) {
+                System.out.println("Lo siento, no conseguiste adivinar el número en 5 intentos. El número era " + numeroAleatorio);
             }
-
         }
 
 
 
 
 
+//        for (int i = 1; i <= 5 ; i++) {
+//
+//            switch (i) {
+//                case 1:
+//                    System.out.println();
+//                    break;
+//                case 2:
+//                    System.out.println();
+//                    break;
+//                case 3:
+//                    System.out.println();
+//                    break;
+//                case 4:
+//                    System.out.println();
+//                    break;
+//                case 5:
+//                    System.out.println();
+//                    break;
+//                default:
+//                    System.out.println("No lograste adivinar el número, juego terminado");
+//            }
+//
+   }
 
-
-
-
-    }
 }
+
